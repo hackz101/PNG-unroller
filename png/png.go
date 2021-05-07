cf5ef2d04b5348a7bcf372a331dd2592523d3f1c
Hayden Baca <therealprogrammer4444@gmail.com>
2021-05-07 11:14:59 -0500

    added error checking for the IHDR chunk processing

--- png/png.go
+++ png/png.go
@@ -68,6 +68,31 @@ func ReadChunk(file *os.File) Chunk {
 	return chunk
 }
 
+func ColorTypeBitDepthCheck(color uint8, bit uint8) bool {
+	if color == 0 {
+		if bit != 1 && bit != 2 && bit != 4 && bit != 8 && bit != 16 {
+			return false
+		}
+	} else if color == 2 {
+		if bit != 8 && bit != 16 {
+			return false
+		}
+	} else if color == 3 {
+		if bit != 1 && bit != 2 && bit != 4 && bit != 8 {
+			return false
+		}
+	} else if color == 4 {
+		if bit != 8 && bit != 16 {
+			return false
+		}
+	} else {
+		if bit != 8 && bit != 16 {
+			return false
+		}
+	}
+	return true
+}
+
 func ProccessChunk(chunk Chunk) {
 	//check chunk type
 	typecode := StringifyType(chunk.typecode)
@@ -80,6 +105,8 @@ func ProccessChunk(chunk Chunk) {
 		fmt.Println("\tCompression method: " + fmt.Sprint(ihdr.compressionmethod))
 		fmt.Println("\tFilter method: " + fmt.Sprint(ihdr.filtermethod))
 		fmt.Println("\tInterlace method: " + fmt.Sprint(ihdr.interlacemethod))
+	} else if typecode == "IDAT" {
+
 	} else {
 		fmt.Println("Sorry haven't implemented this chunk processing yet!")
 	}
@@ -90,11 +117,46 @@ func ProcessIHDR(chunk Chunk) IHDR {
 	var ihdr IHDR
 	ihdr.width = read.ReadUint32Bitstream(&data)
 	ihdr.height = read.ReadUint32Bitstream(&data)
+	//size checks
+	if (ihdr.width == 0 || ihdr.height == 0) || (ihdr.width > uint32(math.Pow(2, 32)-1) || ihdr.height > uint32(math.Pow(2, 32)-1)) {
+		fmt.Println("Image size format error")
+		os.Exit(0)
+	}
 	ihdr.bitdepth = read.ReadUint8Bitstream(&data)
+	//value check
+	if ihdr.bitdepth != 1 && ihdr.bitdepth != 2 && ihdr.bitdepth != 4 && ihdr.bitdepth != 8 && ihdr.bitdepth != 16 {
+		fmt.Println("Bit depth formatting error")
+		os.Exit(0)
+	}
 	ihdr.colortype = read.ReadUint8Bitstream(&data)
+	//value check
+	if ihdr.colortype != 0 && ihdr.colortype != 2 && ihdr.colortype != 3 && ihdr.colortype != 4 && ihdr.colortype != 6 {
+		fmt.Println("Color type formatting error")
+		os.Exit(0)
+	}
+	//is bit depth allowed based on color type?
+	if !(ColorTypeBitDepthCheck(ihdr.colortype, ihdr.bitdepth)) {
+		fmt.Println("Invalid bit depth for color type")
+		os.Exit(0)
+	}
 	ihdr.compressionmethod = read.ReadUint8Bitstream(&data)
+	//make sure method is 0 infalte/deflate
+	if ihdr.compressionmethod != 0 {
+		fmt.Println("Compression method not supported")
+		os.Exit(0)
+	}
 	ihdr.filtermethod = read.ReadUint8Bitstream(&data)
+	//make sure method is 0 adaptive filtering
+	if ihdr.filtermethod != 0 {
+		fmt.Println("Filter method not supported")
+		os.Exit(0)
+	}
 	ihdr.interlacemethod = read.ReadUint8Bitstream(&data)
+	//make sure method is 0 or 1 no interlace or Adam7 interlace
+	if ihdr.interlacemethod != 0 && ihdr.interlacemethod != 1 {
+		fmt.Println("Interlace method not supported")
+		os.Exit(0)
+	}
 	return ihdr
 }
 
